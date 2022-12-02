5dfd3b6b08c167287a9e52515b333b8e5020a92b
Rana Bhat <suraj.ranabhat@sweco.fi>
2022-12-02 23:20:19 +0200

    Database check

--- server/models/carModel.js
+++ server/models/carModel.js
@@ -5,7 +5,7 @@ const promisePool = pool.promise();
 const getAllCars = async () => {
   try {
     // TODO: do the LEFT (or INNER) JOIN to get owner's name as ownername (from wop_user table).
-    const [rows] = await promisePool.query("SELECT * FROM user");
+    const [rows] = await promisePool.query("SELECT * FROM login");
     return rows;
   } catch (e) {
     console.error("error", e.message);
