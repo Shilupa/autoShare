ec5ce5086033f86a76dd228310456c35729b983c
shilpasy <shilpa.yadav@metropolia.fi>
2022-12-02 17:49:55 +0200

    added some routes and dependencies

---
+++ server/routes/authRoute.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('This is auth route!')
+  })
+
+  module.exports = router;
