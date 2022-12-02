9e47ccb6c1ed3f01224f66b289a900a4d11ba5d1
shilpasy <shilpa.yadav@metropolia.fi>
2022-12-02 22:57:10 +0200

    Added controllers,models, database

---
+++ server/models/carModel.js
@@ -0,0 +1,17 @@
+"use strict";
+const pool = require("../database/db");
+const promisePool = pool.promise();
+
+const getAllCars = async () => {
+  try {
+    // TODO: do the LEFT (or INNER) JOIN to get owner's name as ownername (from wop_user table).
+    const [rows] = await promisePool.query("SELECT * FROM user");
+    return rows;
+  } catch (e) {
+    console.error("error", e.message);
+  }
+};
+
+module.exports = {
+  getAllCars,
+};
