71abb68a9b3c8e457ecc85d8e3c966ef878be925
shilpasy <shilpa.yadav@metropolia.fi>
2022-12-05 22:11:09 +0200

    hashed password

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,38 +1,43 @@
-'use strict';
-const userModel = require('../models/userModel');
+"use strict";
+const userModel = require("../models/userModel");
 const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 
 const get_all_users = async (req, response) => {
-    const users = await userModel.getAllUsers();
-    response.json(users);
-  };
+  const users = await userModel.getAllUsers();
+  response.json(users);
+};
 
 const get_user = async (req, res) => {
   const user = await userModel.getUserById(req.params.userId, res);
-  if(user){
+  if (user) {
     res.json(user);
-  } else{
+  } else {
     res.sendStatus(404);
   }
 };
 
-const add_user = async(req, res)=>{
+const add_user = async (req, res) => {
   console.log("creating a new user: ", req.body);
   const newUser = req.body;
-  if (!newUser.role_){
+  if (!newUser.role_) {
     newUser.role_ = "User";
   }
   const errors = validationResult(req);
-  console.log('error: ',errors);
-  
-  if(errors.isEmpty()){
+  console.log("error: ", errors);
+
+  if (errors.isEmpty()) {
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(newUser.password, salt);
+    newUser.password = passwordHash;
     const result = await userModel.addUser(newUser, res);
-    res.status(201).json({message: 'user created', newUserId: result});
-  } else{
-    res.status(400).json({message: 'user creation failed', errors: errors.array()});
+    res.status(201).json({ message: "user created", newUserId: result });
+  } else {
+    res
+      .status(400)
+      .json({ message: "user creation failed", errors: errors.array() });
   }
 };
-
 
 const delete_user = async (req, res) => {
   const result = await userModel.deleteUserById(req.params.userId, res);
@@ -44,28 +49,27 @@ const delete_user = async (req, res) => {
   }
 };
 
-
-const modify_user = async(req, res)=>{
+const modify_user = async (req, res) => {
   const user = req.body;
   user.id = req.params.userId;
-  if (!user.role_){
+  if (!user.role_) {
     user.role_ = "User";
   }
   const result = await userModel.modifyUserById(user, res);
   console.log(result.affectedRows);
-  if(result.affectedRows>0){
-    res.json({message: 'user modified', userId: user.id});
-  } else{
-    res.status(404).json({message: 'There doesnot exist any user with this ID'});
+  if (result.affectedRows > 0) {
+    res.json({ message: "user modified", userId: user.id });
+  } else {
+    res
+      .status(404)
+      .json({ message: "There doesnot exist any user with this ID" });
   }
-  };
-
-
+};
 
 module.exports = {
   get_all_users,
   get_user,
   add_user,
   modify_user,
-  delete_user
+  delete_user,
 };
