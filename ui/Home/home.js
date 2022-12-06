cabdb81a4b9dc22015842bf5e3b3d999a2e3da38
shilpasy <shilpa.yadav@metropolia.fi>
2022-12-06 18:11:12 +0200

    working in home

---
+++ ui/Home/home.js
@@ -0,0 +1,36 @@
+'use strict'
+const url = 'http://localhost:3000';
+
+const addForm = document.querySelector('#addCatForm');
+
+// submit add car form
+addForm.addEventListener('submit', async (evt) => {
+    evt.preventDefault();
+    const fd = new FormData(addForm);
+    const fetchOptions = {
+      method: 'POST',
+      headers: {
+        Authorization: 'Bearer ' + sessionStorage.getItem('token'),
+      },
+      body: fd,
+    };
+    const response = await fetch(url + '/car', fetchOptions);
+    const json = await response.json();
+    alert(json.message);
+  });
+
+const createCarCards = (cars) => {
+    // clear ul
+    ul.innerHTML = '';
+    cars.forEach((car) => {
+      // create li with DOM methods
+      const img = document.createElement('img');
+      img.src = url + '/thumbnails/' + car.filename;
+      img.alt = car.name;
+      img.classList.add('resp');
+  
+      // open image in single.html
+      img.addEventListener('click', () => {
+        location.href = 'single.html?id=' + car.car_id;
+      });
+    })}
