2180597451b2189ec7da7c414714b5dd43aa67a4
Rana Bhat <suraj.ranabhat@sweco.fi>
2022-12-06 17:52:16 +0200

    Car add modify delete

--- server/routes/carRoute.js
+++ server/routes/carRoute.js
@@ -1,14 +1,13 @@
-'use strict';
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const carController = require('../controllers/carController');
+const carController = require("../controllers/carController");
 
 router
-  .get('/',carController.get_home_cars
-  
-  
-  
-  );
-
-  module.exports = router;
+  .get("/", carController.get_home_cars)
+  .get("/:carReg", carController.get_car_by_reg)
+  .post("/", carController.add_car)
+  .put("/:carRegId", carController.modify_car)
+  .delete("/:carRegId", carController.delete_car);
+module.exports = router;
