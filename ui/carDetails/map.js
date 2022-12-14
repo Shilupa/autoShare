6b1567cf10325f00d5e5bdb16d2b793c51b02db4
shilpasy <shilpa.yadav@metropolia.fi>
2022-12-14 16:00:13 +0200

    added google map/converts string location to co-ordinates/ put marker via lat and long in map

--- ui/carDetails/map.js
+++ ui/carDetails/map.js
@@ -1,17 +1,50 @@
-// Initialize and add the map
-function initMap() {
-   // The location of Uluru
-   const uluru = { lat: -25.344, lng: 131.031 };
-   // The map, centered at Uluru
-   const map = new google.maps.Map(document.getElementById("map"), {
-     zoom: 4,
-     center: uluru,
-   });
-   // The marker, positioned at Uluru
-   const marker = new google.maps.Marker({
-     position: uluru,
-     map: map,
-   });
- }
- 
- window.initMap = initMap;
+"use strict";
+const initMap = () => {
+  const geocoder = new google.maps.Geocoder();
+  const infowindow = new google.maps.InfoWindow();
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 8,
+    center: { lat: 40.731, lng: -73.997 },
+  });
+
+  geocoder.geocode({ address: "Maininkitie" }, function (results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      const latitude = results[0].geometry.location.lat();
+      const longitude = results[0].geometry.location.lng();
+
+      document.getElementById("submit").addEventListener("click", () => {
+        geocodeLatLng(geocoder, map, infowindow, latitude, longitude);
+      });
+    }
+  });
+};
+
+const geocodeLatLng = (geocoder, map, infowindow, latitude, longitude) => {
+  /*   const input = document.getElementById("latlng").value;
+  const latlngStr = input.split(",", 2); */
+  const latlng = {
+    lat: latitude,
+    lng: longitude,
+  };
+
+  geocoder
+    .geocode({ location: latlng })
+    .then((response) => {
+      if (response.results[0]) {
+        map.setZoom(11);
+
+        const marker = new google.maps.Marker({
+          position: latlng,
+          map: map,
+        });
+        console.log(response.results[0]);
+        infowindow.setContent(response.results[0].formatted_address);
+        infowindow.open(map, marker);
+      } else {
+        window.alert("No results found");
+      }
+    })
+    .catch((e) => window.alert("Geocoder failed due to: " + e));
+};
+
+window.initMap = initMap;
